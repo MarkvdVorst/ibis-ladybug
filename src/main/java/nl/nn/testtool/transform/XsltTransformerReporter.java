534840772c904d52b48730a9ab0709cb078bdb2e
marcus12000 <marcvorst@gmail.com>
2023-10-18 12:03:31 +0200

    still gives NoSuchMethodError message but progress

--- src/main/java/nl/nn/testtool/transform/XsltTransformerReporter.java
+++ src/main/java/nl/nn/testtool/transform/XsltTransformerReporter.java
@@ -2,31 +2,43 @@ package nl.nn.testtool.transform;
 
 import nl.nn.testtool.TestTool;
 
-import javax.xml.transform.Source;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringWriter;
 
 public class XsltTransformerReporter {
     private TestTool testTool;
-    private Source xmlSource;
-    private Source xslSource;
-    public XsltTransformerReporter(TestTool testTool, Source xmlSource, Source xslSource)
-    {
+    private StreamSource xmlSource;
+    private StreamSource xslSource;
+
+    public XsltTransformerReporter(TestTool testTool, StreamSource xmlSource, StreamSource xslSource) {
         this.testTool = testTool;
         this.xmlSource = xmlSource;
         this.xslSource = xslSource;
     }
-    public void Start(String correlationId, String reportName)
-    {
+
+    public void Start(String correlationId, String reportName) {
         testTool.startpoint(correlationId, null, "XSLT start point", "XSLT start point");
 
-        //some method calls for all the input
-
+        try {
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer transformer = tFactory.newTransformer();
+            transformer.transform(xmlSource, result);
+            testTool.infopoint(correlationId, null, "XML Source", result.toString());
+            transformer.transform(xslSource, result);
+            testTool.infopoint(correlationId, null, "XML Source", result.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         testTool.endpoint(correlationId, null, "XSLT end point", "XSLT end point");
     }
 
-    private void LoopTemplates(String correlationId)
-    {
+    private void LoopTemplates(String correlationId) {
         //for templateName in xslSource
-            //testTool.startpoint(correlationId, null, "start of template: {templateName}", "{templateName}")
-            //
+        //testTool.startpoint(correlationId, null, "start of template: {templateName}", "{templateName}")
+        //
     }
 }
