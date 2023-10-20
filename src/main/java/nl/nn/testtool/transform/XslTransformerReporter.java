f86a32fb6ab6ebae04a9a27f0a0c98cb67d88d93
DoNotTrustWeebs <marcvorst@gmail.com>
2023-10-20 14:31:37 +0200

    20 oktober update. Als gebruiker wil ik de output van de XSLT zien zodat ik weet of de data juist is verwerkt door de template. op review/testing gezet

--- src/main/java/nl/nn/testtool/transform/XslTransformerReporter.java
+++ src/main/java/nl/nn/testtool/transform/XslTransformerReporter.java
@@ -1,25 +1,22 @@
 package nl.nn.testtool.transform;
 
 import nl.nn.testtool.TestTool;
+import org.apache.xalan.processor.TransformerFactoryImpl;
+import org.apache.xalan.trace.PrintTraceListener;
+import org.apache.xalan.trace.TraceManager;
+import org.apache.xalan.transformer.TransformerImpl;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
-import javax.lang.model.util.Elements;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.URI;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class XslTransformerReporter {
     private TestTool testTool;
@@ -49,46 +46,74 @@ public class XslTransformerReporter {
             testTool.infopoint(correlationId, null, "XSL Source", writer.toString());
 
             GetAllImportedXSL(correlationId);
+
+            GetCompleteXSLT(correlationId);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         testTool.endpoint(correlationId, null, "XSLT end point", "XSLT end point");
     }
 
-    private void GetAllImportedXSL(String correlationId)
-    {
+    private void GetAllImportedXSL(String correlationId) {
         try {
-            InputSource inputSource = new InputSource();
-            inputSource.setSystemId(xslSource.getSystemId());
             File file = new File(xslSource.getSystemId());
-            InputStream inputStream = new FileInputStream(file);
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document doc = documentBuilder.parse(inputStream);
-            XPath xPath = XPathFactory.newInstance().newXPath();
-            NodeList nodeList = (NodeList) xPath.compile("//xsl:import").evaluate(doc, XPathConstants.NODESET);
+            Document doc = documentBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("xsl:import");
+            if (nodeList.getLength() > 0) {
+                testTool.startpoint(correlationId, null, "Imported XSL", "Imported XSL");
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Element element = (Element) nodeList.item(i);
+                    String importPath = element.getAttribute("href");
+                    Path path = Paths.get(importPath);
+                    String fileName = path.getFileName().toString();
 
-            StringWriter writer = new StringWriter();
-            StreamResult result = new StreamResult(writer);
-            TransformerFactory tFactory = TransformerFactory.newInstance();
-            Transformer transformer = tFactory.newTransformer();
+                    StringWriter writer = new StringWriter();
+                    StreamResult result = new StreamResult(writer);
+                    TransformerFactory tFactory = TransformerFactory.newInstance();
+                    Transformer transformer = tFactory.newTransformer();
+                    transformer.transform(new StreamSource(path.toString()), result);
 
-            for(int i = 0; i < nodeList.getLength(); i++){
-                Element element = (Element) nodeList.item(i);
-                String importName = element.getAttribute("href");
-                testTool.infopoint(correlationId, null, importName, "test");
+                    testTool.infopoint(correlationId, null, fileName, writer.toString());
+                    writer = new StringWriter();
+                    result.setWriter(writer);
+                }
+                testTool.endpoint(correlationId, null, "Imported XSL", "Imported XSL");
             }
-
-            testTool.infopoint(correlationId, null, "amount of imports", nodeList.getLength());
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void LoopTemplates(String correlationId) {
-        //for templateName in xslSource
-        //testTool.startpoint(correlationId, null, "start of template: {templateName}", "{templateName}")
-        //
+    private void GetCompleteXSLT(String correlationId)
+    {
+        Writer writer = testTool.infopoint(correlationId, null, "Complete XSLT", new StringWriter());
+        PrintWriter printWriter = new PrintWriter(writer);
+        PrintTraceListener printTraceListener = new PrintTraceListener(printWriter);
+
+        printTraceListener.m_traceElements = true;
+        printTraceListener.m_traceGeneration = true;
+        printTraceListener.m_traceSelection = true;
+        printTraceListener.m_traceTemplates = true;
+        printTraceListener.m_traceExtension = true;
+
+        try {
+            TransformerFactoryImpl transformerFactory = new TransformerFactoryImpl();
+            Transformer transformer = transformerFactory.newTransformer(xslSource);
+
+            TransformerImpl transformerImpl = (TransformerImpl) transformer;
+            TraceManager traceManager = transformerImpl.getTraceManager();
+            traceManager.addTraceListener(printTraceListener);
+
+            Writer resultWriter = testTool.infopoint(correlationId, null, "XSLT Result", new StringWriter());
+            transformer.transform(xmlSource, new StreamResult(resultWriter));
+
+            printWriter.close();
+            resultWriter.close();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
